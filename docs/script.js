42b635c9b546ff039012e8c2f17fc2152a6b7dc3
minhmc2007 <quangminh21072010@gmail.com>
2025-07-24 10:45:47 +0700

    update website

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,37 @@
 const themeSwitcher = document.getElementById('theme-switcher');
+const navToggle = document.getElementById('nav-toggle');
+const navLinks = document.getElementById('nav-links');
 
 themeSwitcher.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
+});
+
+navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    navToggle.classList.toggle('active');
+});
+
+// Animations on scroll
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll('section');
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('is-visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    sections.forEach(section => {
+        section.classList.add('fade-in-section');
+        observer.observe(section);
+    });
 });
